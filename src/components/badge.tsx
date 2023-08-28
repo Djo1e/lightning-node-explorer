1edb3dd58ebb1b4381a23fbc474ca19971659500
djo1e <djordjespasic7@gmail.com>
2023-08-28 12:56:17 +0200

    Create search UI

---
+++ src/components/badge.tsx
@@ -0,0 +1,11 @@
+type BadgeProps = {
+  children: React.ReactNode;
+};
+
+export function Badge({ children }: BadgeProps) {
+  return (
+    <span className="mt-4 rounded-md text-center bg-blue-100 px-4 py-1 text-lg font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
+      {children}
+    </span>
+  );
+}
---
+++ src/components/main-search.tsx
@@ -0,0 +1,28 @@
+type MainSearchProps = {
+  value: string;
+  setValue: (value: string) => void;
+};
+
+export function MainSearch({ value, setValue }: MainSearchProps) {
+  return (
+    <div className="relative flex flex-col mt-10 w-full sm:flex-row sm:w-1/2">
+      <div className="relative w-full">
+        <input
+          className="w-full text-lg text-slate-900 px-4 sm:pl-12 rounded sm:rounded-r-none py-2 border"
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+          placeholder="Enter a node pubkey"
+        />
+        <span className="hidden sm:inline absolute top-1/2 -translate-y-1/2 left-3 text-xl">
+          🔍
+        </span>
+      </div>
+      <button
+        className="px-8 mt-2 py-2 sm:mt-0 bg-blue-500 text-white rounded sm:rounded-l-none text-lg"
+        type="submit"
+      >
+        Search
+      </button>
+    </div>
+  );
+}
